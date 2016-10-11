599402ce56f63b309673afdca9fa10c4edaf7798
jeroendedauw <jeroendedauw@gmail.com>
2016-10-11 10:16:49 +0200

    Added getIndexForName to PlayerArmies

--- spec/Test_spec.lua
+++ spec/Test_spec.lua
@@ -3,21 +3,18 @@ describe("PlayerArmies", function()
     local PlayerArmies = require 'PlayerArmies'
 
     describe("getNameForIndex", function()
-
         local armies = PlayerArmies({[1] = "ARMY_1", [2] = "ARMY_2"})
 
         it("Given known army index, army name is returned", function()
             assert.are.equal("ARMY_2", armies.getNameForIndex(2))
         end)
 
-        it("Given unknown army index, army nil is returned", function()
+        it("Given unknown army index, nil is returned", function()
             assert.is.is_nil(armies.getNameForIndex(3))
         end)
-
     end)
 
     describe("getIndexToNameMap", function()
-
         it("Given empty collection, empty map is returned", function()
             assert.are.same({}, PlayerArmies({}).getIndexToNameMap())
         end)
@@ -26,7 +23,19 @@ describe("PlayerArmies", function()
             local armyMap = {[1] = "ARMY_1", [2] = "ARMY_2"}
             assert.are.same(armyMap, PlayerArmies(armyMap).getIndexToNameMap())
         end)
+    end)
 
+    describe("getIndexForName", function()
+        it("Given unknown army name, nil is returned", function()
+            assert.is.is_nil(PlayerArmies({}).getIndexForName("ARMY_1"))
+        end)
+
+        it("Given known army name, its index is returned", function()
+            assert.is.equal(
+                2,
+                PlayerArmies({[1] = "ARMY_1", [2] = "ARMY_5"}).getIndexForName("ARMY_5")
+            )
+        end)
     end)
 
 end)
--- src/PlayerArmies.lua
+++ src/PlayerArmies.lua
@@ -1,12 +1,28 @@
-return function(armies)
+return function(indexToNameMap)
     local this = {}
+    local nameToIndexMap
 
     this.getNameForIndex = function(armyIndex)
-        return armies[armyIndex]
+        return indexToNameMap[armyIndex]
     end
 
     this.getIndexToNameMap = function()
-        return armies
+        return indexToNameMap
+    end
+
+    local function buildNameToIndexMap()
+        if nameToIndexMap == nil then
+            nameToIndexMap = {}
+
+            for armyIndex, armyName in pairs(indexToNameMap) do
+                nameToIndexMap[armyName] = armyIndex
+            end
+        end
+    end
+
+    this.getIndexForName = function(armyName)
+        buildNameToIndexMap()
+        return nameToIndexMap[armyName]
     end
 
     return this
