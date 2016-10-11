0fe77c4fddb2a661ec00635946bbb98b74967e85
jeroendedauw <jeroendedauw@gmail.com>
2016-10-11 09:09:40 +0200

    Added some try-out code and test for an army list wrapper

--- spec/Test_spec.lua
+++ spec/Test_spec.lua
@@ -1,7 +1,46 @@
+PlayerArmies = {}
+
+function PlayerArmies:new(armies)
+    self._armies = armies
+    return self
+end
+
+function PlayerArmies:getNameForIndex(armyIndex)
+    return self._armies[armyIndex]
+end
+
+function PlayerArmies:getIndexToNameMap()
+    return self._armies
+end
 
 
-describe("a test", function()
-    it("tests positive assertions", function()
-        assert.are.equal(1, 1)
+describe("ArmyList", function()
+
+    describe("getNameForIndex", function()
+
+        local armies = PlayerArmies:new({[1] = "ARMY_1", [2] = "ARMY_2"})
+
+        it("Given known army index, army name is returned", function()
+            assert.are.equal("ARMY_2", armies:getNameForIndex(2))
+        end)
+
+        it("Given unknown army index, army nil is returned", function()
+            assert.is.is_nil(armies:getNameForIndex(3))
+        end)
+
     end)
+
+    describe("getIndexToNameMap", function()
+
+        it("Given empty collection, empty map is returned", function()
+            assert.are.same({}, PlayerArmies:new({}):getIndexToNameMap())
+        end)
+
+        it("Given map in constructor, same map is returned", function()
+            local armyMap = {[1] = "ARMY_1", [2] = "ARMY_2"}
+            assert.are.same(armyMap, PlayerArmies:new(armyMap):getIndexToNameMap())
+        end)
+
+    end)
+
 end)
