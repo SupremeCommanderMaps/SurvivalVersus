d55b75fd84600aa20d8b1d90bdc0791ddaa57c1f
Jeroen De Dauw <jeroendedauw@gmail.com>
2016-11-09 06:20:52 +0100

    Removed not needed local var

--- Final Rush Pro 5_script.lua
+++ Final Rush Pro 5_script.lua
@@ -1,9 +1,5 @@
-local ScenarioUtils = import('/lua/sim/ScenarioUtilities.lua');
-
-
-
 function OnPopulate()
-	ScenarioUtils.InitializeArmies()
+	import('/lua/sim/ScenarioUtilities.lua').InitializeArmies()
 
 	local Options = import('/maps/Final Rush Pro 5/src/FinalRushOptions.lua')
 	ScenarioInfo.Options = Options.applyPresets(Options.defaultOptions(ScenarioInfo.Options))
