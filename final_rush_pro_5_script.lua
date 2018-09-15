f8f99a215c1c6c3fd0d699e841663ba5435e7f03
Jeroen De Dauw <jeroendedauw@gmail.com>
2018-09-15 20:03:23 +0200

    Remove not needed import

--- final_rush_pro_5_script.lua
+++ final_rush_pro_5_script.lua
@@ -1,8 +1,6 @@
 function OnPopulate()
 	import('/lua/sim/ScenarioUtilities.lua').InitializeArmies()
 	import('/maps/final_rush_pro_5.v0017/src/frp/FinalRushPro.lua').newInstance(ScenarioInfo).setUp()
-
-	import('/lua/SimSync.lua')
 end
 
 --                    local keyset={}
